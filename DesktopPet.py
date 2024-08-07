81e3d1461efcbf57b5ca5f5aa19c3488b6ab78ae
Daniel Lee <96561755+eel-in-denial@users.noreply.github.com>
2024-08-07 20:47:32 +0930

    Update DesktopPet.py

--- DesktopPet.py
+++ DesktopPet.py
@@ -13,7 +13,7 @@ class Path():
         self.finish = []
         
 class Spine():
-    def __init__(self, circle_no, radi, sizes):
+    def __init__(self, circle_no, radi, sizes, mouse_x, mouse_y):
         self.points_list = []
         self.radi_list = radi
         self.sizes_list = sizes
@@ -28,7 +28,7 @@ class Spine():
         self.eyes = []
         self.eyes_canvas = []
         for i in range(circle_no):
-            self.points_list.append([50 + i*self.radi_list[i], 50])
+            self.points_list.append([mouse_x + i*self.radi_list[i], mouse_y])
             # body and rigging
             # drawing_points = [self.points_list[i][0]-self.radi_list[i], self.points_list[i][1]-self.radi_list[i], self.points_list[i][0]+self.radi_list[i], self.points_list[i][1]+self.radi_list[i]]
             # circle = canvas.create_oval(drawing_points, outline='blue', width=2)
@@ -47,12 +47,12 @@ class Spine():
             self.body_points_left.append([self.points_list[i][0] + math.cos(self.direction[i]-pi/2)*self.sizes_list[i], self.points_list[i][1] + math.sin(self.direction[i]-pi/2)*self.sizes_list[i]])
             self.body_points_right.append([self.points_list[i][0] + math.cos(self.direction[i]+pi/2)*self.sizes_list[i], self.points_list[i][1] + math.sin(self.direction[i]+pi/2)*self.sizes_list[i]])
             #side circles drawn
-            # drawing_points = [self.body_points_left[i][0]-5, self.body_points_left[i][1]-5, self.body_points_left[i][0]+5, self.body_points_left[i][1]+5]
-            # circle = canvas.create_oval(drawing_points, outline='red', width=2)
-            # self.side_canvas.append(circle)
-            # drawing_points = [self.body_points_right[i][0]-5, self.body_points_right[i][1]-5, self.body_points_right[i][0]+5, self.body_points_right[i][1]+5]
-            # circle = canvas.create_oval(drawing_points, outline='orange', width=2)
-            # self.side_canvas.append(circle)
+            drawing_points = [self.body_points_left[i][0]-5, self.body_points_left[i][1]-5, self.body_points_left[i][0]+5, self.body_points_left[i][1]+5]
+            circle = canvas.create_oval(drawing_points, outline='red', width=2)
+            self.side_canvas.append(circle)
+            drawing_points = [self.body_points_right[i][0]-5, self.body_points_right[i][1]-5, self.body_points_right[i][0]+5, self.body_points_right[i][1]+5]
+            circle = canvas.create_oval(drawing_points, outline='orange', width=2)
+            self.side_canvas.append(circle)
         # polygon
         self.body_points_right.reverse()
         self.body = canvas.create_polygon(self.body_points_left + self.body_points_right, outline="white", width=2, smooth=1, fill="yellow")
@@ -63,14 +63,14 @@ class Spine():
         drawing_points = [self.eyes[1][0]-10, self.eyes[1][1]-10, self.eyes[1][0]+10, self.eyes[1][1]+10]
         eye = canvas.create_oval(drawing_points, outline='black', width=2, fill="black")
         self.eyes_canvas.append(eye)
-        window.after(10, programloop)
     def update(self, mouse_x, mouse_y):
         for i in range(len(self.points_list)):
             sqrt = math.sqrt((self.points_list[i][0] - self.points_list[i-1][0])**2 + (self.points_list[i][1] - self.points_list[i-1][1])**2)
             if i == 0:
                 direction_x, direction_y = mouse_x - self.points_list[0][0], mouse_y - self.points_list[0][1]
+                self.direction[i] = math.atan2(direction_y, direction_x)
+                print(self.direction[i])
                 self.points_list[0][0], self.points_list[0][1] = mouse_x, mouse_y
-                length = self.sizes_list[i]/sqrt
                 self.head[0] = [self.points_list[i][0] + math.cos(self.direction[i]+pi/6)*60, self.points_list[i][1] + math.sin(self.direction[i]+pi/6)*60]
                 self.head[1] = [self.points_list[i][0] + math.cos(self.direction[i]-pi/6)*60, self.points_list[i][1] + math.sin(self.direction[i]-pi/6)*60]
                 self.eyes[0] = [self.points_list[i][0] + math.cos(self.direction[i]+4*pi/3)*30, self.points_list[i][1] + math.sin(self.direction[i]+4*pi/3)*30]
@@ -78,17 +78,21 @@ class Spine():
             else:
                 length = 1-(self.radi_list[i]/sqrt)
                 direction_x, direction_y = self.points_list[i-1][0] - self.points_list[i][0], self.points_list[i-1][1] - self.points_list[i][1]
+                self.direction[i] = math.atan2(direction_y, direction_x)
                 self.points_list[i][0] += length*(direction_x)
                 self.points_list[i][1] += length*(direction_y)
             # calculates position of points
-            self.direction[i] = math.atan2(direction_y, direction_x)
             self.body_points_left[i] = [self.points_list[i][0] + math.cos(self.direction[i]-pi/2)*self.sizes_list[i], self.points_list[i][1] + math.sin(self.direction[i]-pi/2)*self.sizes_list[i]]
             self.body_points_right[i] = [self.points_list[i][0] + math.cos(self.direction[i]+pi/2)*self.sizes_list[i], self.points_list[i][1] + math.sin(self.direction[i]+pi/2)*self.sizes_list[i]]
             # moves the side circles
-            # drawing_points = [self.body_points_left[i][0]-5, self.body_points_left[i][1]-5]
-            # canvas.moveto(self.side_canvas[i*2], *drawing_points)
-            # drawing_points = [self.body_points_right[i][0]-5, self.body_points_right[i][1]-5]
-            # canvas.moveto(self.side_canvas[i*2+1], *drawing_points)
+            drawing_points = [self.body_points_left[i][0]-5, self.body_points_left[i][1]-5]
+            canvas.moveto(self.side_canvas[i*2], *drawing_points)
+            drawing_points = [self.body_points_right[i][0]-5, self.body_points_right[i][1]-5]
+            canvas.moveto(self.side_canvas[i*2+1], *drawing_points)
+            # if i == 0:
+            #     print(self.direction[i])
+            #     print(self.direction[i]-pi/2)
+            #     print(self.direction[i]+pi/2)
 
             # moves rigging circles
             # canvas.moveto(self.rigged_canvas[i], int(self.points_list[i][0] - self.radi_list[i]), int(self.points_list[i][1] - self.radi_list[i]))
@@ -101,6 +105,10 @@ class Spine():
         self.body_points_right.reverse()
         canvas.moveto(self.eyes_canvas[0], self.eyes[0][0] - 10, self.eyes[0][1] - 10)
         canvas.moveto(self.eyes_canvas[1], self.eyes[1][0] - 10, self.eyes[1][1] - 10)
+    def __del__(self):
+        canvas.delete(self.body)
+        canvas.delete(self.eyes_canvas[0])
+        canvas.delete(self.eyes_canvas[1])
 
  # detect perpendicular and then create polygon
 class Pet():
@@ -117,13 +125,22 @@ class Pet():
         self.length = 0
         radi = [40, 40, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
         bodies = [35, 45, 35, 30, 30, 29, 29, 28, 28, 27, 27, 26, 26, 25, 25, 24, 24, 23, 23, 22, 22, 21, 21, 20, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5]
-        self.spine = Spine(40, radi, bodies)
+        self.spine = Spine(40, radi, bodies, window.winfo_pointerx(), window.winfo_pointery())
         window.after(100, programloop)
     def update(self):
         self.spine.update(window.winfo_pointerx(), window.winfo_pointery())
+    def __del__(self):
+        del self.spine
+
 
 class UI():
     def __init__(self):
+        self.create_buttons()
+    def create_buttons(self):
+        global pet
+        if pet != 0:
+            del pet
+        pet = 0
         self.buttons = []
         button = tk.Button(canvas, height=1, width=10, bg="red", command=self.pet_select, text="Pet Select", fg="white", font="Arial 18 bold")
         button.place(x=750, y=750)
@@ -169,11 +186,11 @@ class UI():
 def programloop(): # main loop
     if pet != 0:
         pet.update()
-    window.after(10, programloop)
+    window.after(1000, programloop)
 
 def check_key(event): # check what key was pressed
     if event.keysym == "p":
-        window.destroy() # close program
+        ui.create_buttons()
 
 # creating window / fullscreen / top overlay
 window = tk.Tk()
